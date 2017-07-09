9eefb5efae0c4c3e75a3cc56fd20fd820a1ddaf3
igor-sfdc <igor-sfdc@users.noreply.github.com>
2017-07-09 12:35:22 -0700

    Forked and added custom room scripts

---
+++ turnRoomsOff.py
@@ -0,0 +1,8 @@
+import pygop
+
+pygop = pygop.pygop()
+
+# Turns rooms OFF
+pygop.setRoomLevelByRid(1, 0)
+pygop.setRoomLevelByRid(4, 0)
+pygop.setRoomLevelByRid(5, 0)
---
+++ turnRoomsOn.py
@@ -0,0 +1,8 @@
+import pygop
+
+pygop = pygop.pygop()
+
+# Turns rooms ON
+pygop.setRoomLevelByRid(1, 1)
+pygop.setRoomLevelByRid(4, 1)
+pygop.setRoomLevelByRid(5, 1)
