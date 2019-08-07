d271b1a3eddb40ef2555df5a9a9f0be7dcb16f38
Igor Shmulevich <igor-sfdc@users.noreply.github.com>
2019-08-07 15:55:23 -0700

    added config options

--- setDeskLight50.py
+++ setDeskLight50.py
@@ -1,7 +1,15 @@
 import pygop
+import sys
+from ConfigParser import SafeConfigParser
+
+# Read config options
+config = SafeConfigParser()
+config.read('config')
+deskLightDid = int(config.get('main', 'deskLightDid'))
+deskLightBrightness = int(config.get('main', 'deskLightBrightness'))
 
 pygop = pygop.pygop()
 
-# Set desk lapmp to DIM=50
-# TODO: need to use room id instead device id
-pygop.setBulbLevelByDid(216593688913573236, 1, 50)
+# Set desk lapmp to the value specified in the config file (1-100, not validated) or default to 50
+brightness = deskLightBrightness if (deskLightBrightness > 0 and deskLightBrightness <= 100) else 50
+pygop.setBulbLevelByDid(deskLightDid, 1, brightness)
